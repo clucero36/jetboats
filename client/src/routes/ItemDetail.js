0428f8fa7a2b03996e07e9b423cdd5c7af7f2e15
Luis Lucero <clucero3696@gmail.com>
2022-08-08 12:42:37 -0700

    Cart Component & Image Updates

--- client/src/routes/ItemDetail.js
+++ client/src/routes/ItemDetail.js
@@ -1,18 +1,12 @@
-import React, { useContext } from 'react';
+import React from 'react';
 import { useSearchParams } from "react-router-dom"
-import { CartContext } from '../Context';
 import {
   Box,
-  Text,
-  HStack,
-  VStack,
 } from '@chakra-ui/react';
 import NavBar from '../components/NavBar';
 
 const ItemDetail = () => {
 
-  const {setCart} = useContext(CartContext);
-  const [searchParams] = useSearchParams();
   const item_id = searchParams.get('id');
   console.log(item_id)
 
