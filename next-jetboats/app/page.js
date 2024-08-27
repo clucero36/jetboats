306a5e21e278aa694319c717f6d0cdb1120be3c6
llucero <clucero3696@gmail.com>
2024-08-27 10:40:07 -0700

    Product Page & shopping cart quantity

--- next-jetboats/app/page.js
+++ next-jetboats/app/page.js
@@ -7,14 +7,16 @@ export default function Home() {
   return (
     <main>
       <div className="relative isolate overflow-hidden bg-gray-900 py-24 sm:py-32">
-        <img
+        <Image
+          width={2000}
+          height={1000}
           alt=""
-          src="jetboats.webp"
+          src="/jetboats.webp"
           className="absolute inset-0 -z-10 h-full w-full object-cover object-right md:object-center"
         />
         <div className="mx-auto max-w-md px-6 lg:px-8">
           <div className="mx-auto lg:mx-0 text-center">
-            <h2 className="text-6xl font-bold tracking-tight text-white "><span className={nerko_one.className}>Jet Boat Classifieds</span></h2>
+            <h2 className={`text-6xl font-bold tracking-tight text-white ${nerko_one.className}`}>Jet Boat Classifieds</h2>
           </div>
         </div>
       </div>
--- next-jetboats/app/ui/shoppingcart.js
+++ next-jetboats/app/ui/shoppingcart.js
@@ -10,23 +10,30 @@ import Image from 'next/image';
 export default function ShoppingCart() {
   const [open, setOpen] = useState(false)
   const {cart, setCart} = useContext(CartContext);
+  const [totalQuantity, setTotalQuanity] = useState(0);
   const [totalPrice, setTotalPrice] = useState(0);
 
   useEffect(() => {
     let price = 0;
+    let totalQuantity = 0;
     cart.forEach((cItem) => {
       price += (cItem.price_in_cents/100)*cItem.quantity;
+      totalQuantity += cItem.quantity;
     })
     setTotalPrice(price);
+    setTotalQuanity(totalQuantity);
   }, [cart])
 
   return (
     <>
-      <button onClick={() => setOpen(true)}>
-        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
-          <path strokeLinecap="round" strokeLinejoin="round" d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 0 0-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 0 0-16.536-1.84M7.5 14.25 5.106 5.272M6 20.25a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Zm12.75 0a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Z" />
-        </svg>
-      </button>
+      <div className='flex'>
+        <span>{totalQuantity}</span>
+        <button onClick={() => setOpen(true)}>
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+            <path strokeLinecap="round" strokeLinejoin="round" d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 0 0-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 0 0-16.536-1.84M7.5 14.25 5.106 5.272M6 20.25a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Zm12.75 0a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Z" />
+          </svg>
+        </button>
+      </div>
       <Dialog open={open} onClose={setOpen} className="relative z-10">
         <DialogBackdrop
           transition
