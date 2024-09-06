021206f4602793503c2a6e65e419d340162d1f90
llucero <clucero3696@gmail.com>
2024-09-06 11:38:57 -0700

    Firebase Project2 for Refactor

---
+++ firebase2/functions/index.js
@@ -0,0 +1,102 @@
+const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+admin.initializeApp();
+
+
+// Access firestore database & return contents
+exports.getFirestore = functions.https.onRequest(async (req, res) => {
+  res.set('Access-Control-Allow-Origin', '*');
+  const { getFirestore } = require('firebase-admin/firestore');
+  try {
+    const db = getFirestore();
+    const shopItemsRef = db.collection('shopItems');
+    const snapshot = await shopItemsRef.get();   
+    let shopItems = []
+    snapshot.forEach((doc) => {
+      shopItems.push(doc.data())
+    })
+    res.status(200).send(shopItems);
+  } catch (error) {
+    console.error('Error fetching shop items:', error);
+    res.status(500).json({error: error.message})
+  }
+})
+
+// https endpoint creates stripe checkout session
+exports.createCheckoutSession = functions.https.onRequest(async (req, res) => {
+  res.set('Access-Control-Allow-Origin', '*');
+  res.set('Access-Control-Allow-Headers', '*');
+  const stripe = require("stripe")(functions.config().stripe.s_key);
+  try {
+    const session = await stripe.checkout.sessions.create({
+      payment_method_types: ['card'],
+      mode: 'payment',
+      shipping_address_collection: {
+        allowed_countries: ['US'],
+      },
+      line_items: req.body.cart.map(cItem => {
+        return {
+          price_data: {
+            currency: 'usd',
+            product_data: {
+              name: cItem.name,
+              metadata: {
+                size: cItem.purchaseSize,
+              },
+            },
+            unit_amount: cItem.price_in_cents,
+          },
+          quantity: cItem.quantity,
+        }
+      }),
+      success_url: `${functions.config().stripe.client_url}/success?session_id={CHECKOUT_SESSION_ID}`,
+      cancel_url: `${functions.config().stripe.client_url}/cancel?session_id={CHECKOUT_SESSION_ID}`,
+    });
+    res.send({url: await session.url});
+  } catch (e) {
+    res.json({error: e.message});
+  }
+})
+
+// request session id for checkout success page
+exports.getSession = functions.https.onRequest(async (req, res) => {
+  res.set('Access-Control-Allow-Origin', '*');
+  res.set('Access-Control-Allow-Headers', '*');
+  res.setHeader("Access-Control-Allow-Methods", "GET");
+  const stripe = require("stripe")(functions.config().stripe.s_key);
+  try {
+    const session = await stripe.checkout.sessions.retrieve(req.headers.session_id);
+    res.send({session: await session});
+  } catch (e) {
+    res.json({error: e.message});
+  }
+});
+
+// request items purchased for checkout success page
+exports.getLineItems = functions.https.onRequest(async (req, res) => {
+  res.set('Access-Control-Allow-Origin', '*');
+  res.set('Access-Control-Allow-Headers', '*');
+  res.set("Access-Control-Allow-Methods", "GET");
+  const stripe = require("stripe")(functions.config().stripe.s_key);
+  try {
+    const lineItems = await stripe.checkout.sessions.listLineItems(req.headers.session_id, {expand: ['data.price.product']});
+    res.send({lineItems: await lineItems})
+  } catch (e) {
+    res.json({error: e.message});
+  }
+})
+
+// expire a session when checkout is canceled
+exports.expireSession = functions.https.onRequest(async (req, res) => {
+  res.set('Access-Control-Allow-Origin', '*');
+  res.set('Access-Control-Allow-Headers', '*');
+  res.set("Access-Control-Allow-Methods", "POST");
+  const stripe = require("stripe")(functions.config().stripe.s_key);
+  try {
+    const session = await stripe.checkout.sessions.expire(req.headers.session_id);
+    res.send(session);
+  } catch (e) {
+    res.json({error: e.message});
+  }
+})
+
