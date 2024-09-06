40a1bf9f751cbd5915c4972000db133dcff4c75a
llucero <clucero3696@gmail.com>
2024-09-06 15:01:57 -0700

    Firestore fetch shopItems

--- next-jetboats/app/ui/products.js
+++ next-jetboats/app/ui/products.js
@@ -1,26 +1,38 @@
-import { products } from "../lib/data";
 import Image from "next/image";
 import Link from "next/link";
 
-export default function Proudcts() {
+export default async function Proudcts() {
+  var shopItems;
+  
+  try {
+    const response = await fetch('https://us-central1-jetboats.cloudfunctions.net/getFirestore');
+  
+    if (response.ok) {
+      shopItems = await response.json();
+    } else {
+      throw new Error(response.status);
+    }
+  } catch (error) {
+    console.error('Fetch', error);
+  }
 
   return (
     <div className="mx-auto px-4 py-8 sm:px-6 lg:max-w-7xl lg:px-8">
       <h2 className="sr-only">Products</h2>
       <div className="grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
-        {products.map((product) => (
+        {shopItems.map((product) => (
           <Link key={product.name} href={`/product?name=${product.name}`} className="group">
             <div className="relative aspect-[3/4] w-full overflow-hidden rounded-lg bg-gray-200">
-              <Image
-                width={1000}
-                height={1000}
-                alt={product.description}
-                src={product.img}
-                className="h-full w-full object-cover object-center group-hover:opacity-75"
-              />
+            <Image
+              width={1000}
+              height={1000}
+              alt={product.description}
+              src={product.image_src.includes('https') ? product.image_src : `/${product.image_src}`}
+              className="h-full w-full object-cover object-center group-hover:opacity-75"
+            />
             </div>
             <h3 className="mt-4 text-sm text-gray-700">{product.name}</h3>
-            <p className="mt-1 text-lg font-medium text-gray-900">{product.price}</p>
+            <p className="mt-1 text-lg font-medium text-gray-900">${parseInt(product.price_in_cents)/100}</p>
           </Link>
         ))}
       </div>
