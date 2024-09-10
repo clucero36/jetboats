c52a2e937f5b36d11f95039d517115efd4be39ab
llucero <clucero3696@gmail.com>
2024-09-09 17:04:28 -0700

    escaped char

--- next-jetboats/app/success/page.js
+++ next-jetboats/app/success/page.js
@@ -13,7 +13,7 @@ export default async function Page({ searchParams }) {
         <div className="mx-auto max-w-3xl px-4 py-16 sm:px-6 sm:py-24 lg:px-8">
           <div className="max-w-xl">
             <h1 className="text-base font-medium text-indigo-600">Thank you!</h1>
-            <p className="mt-2 text-4xl font-bold tracking-tight sm:text-5xl">It's on the way!</p>
+            <p className="mt-2 text-4xl font-bold tracking-tight sm:text-5xl">It&apos;s on the way!</p>
             <p className="mt-2 text-base text-gray-500">Your order #{checkoutSession.id.slice(8)} has shipped and will be with you soon.</p>
           </div>
   
