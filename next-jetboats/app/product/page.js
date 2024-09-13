21149d0e29f1a89c993ddda2a7c115db4663cafc
llucero <clucero3696@gmail.com>
2024-09-12 17:50:58 -0700

    firestore data errors

--- next-jetboats/app/product/page.js
+++ next-jetboats/app/product/page.js
@@ -3,11 +3,15 @@ import { fetchCurrentProductData } from "../lib/data";
 import { Suspense } from "react";
 import Loading from "./loading";
 
+export const dynamic = 'force-dynamic'
+
 export default async function Page({ searchParams }) {
 
   const shopItemName = searchParams.name;
 
   const { currProduct, currReviews, currFaqs } = await fetchCurrentProductData(shopItemName);
+
+  console.log(currProduct, currReviews, currFaqs);
 
   return (
     <div>
